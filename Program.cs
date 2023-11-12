4b7ea9e13ac1f2f63e2a733fc75f800c71deaec8
jcampbel2-kings <james.campbell@kingsacademies.uk>
2023-11-12 17:02:22 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿using System;
+using System.ComponentModel.Design.Serialization;
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        //generate a random list of integers 
+        static List<int> GenerateList(int size, int low, int high){
+            Random rand = new Random();
+            List<int> retList= new List<int> {};
+            for (int i=0; i<size; i++){
+                retList.Add(rand.Next(low,high));
+            }
+            return retList;
+        }
+
+        static void Main(string[] args){
+            List<int> rndList=GenerateList(10,0,100);
+            
+        }
+
+
+    }
+}
