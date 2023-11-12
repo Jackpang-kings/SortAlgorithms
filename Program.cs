ac62d49fd4f1dd79db0eafed2733d85f3a082496
jcampbel2-kings <james.campbell@kingsacademies.uk>
2023-11-12 17:31:39 +0000

    Skeletons created

--- Program.cs
+++ Program.cs
@@ -14,9 +14,48 @@ namespace SortingAlgorithms
             return retList;
         }
 
+        static List<int> MyBubbleSort(List<int> inList){
+            List<int> retList=new List<int>{};
+
+
+            return retList;
+        }
+
+        //Helper function that can merge 2 lists into a new one in sorted order
+        static List<int> MyMergeLists(List<int> listA, List<int> listB){
+            List<int> retList=new List<int>{};
+
+
+            return retList;            
+        }
+
+        //main function, designed to be used recursively
+        static List<int> MyMergeSort(List<int> inList){
+            List<int> retList=new List<int>{};
+
+
+            return retList;
+        }
+
         static void Main(string[] args){
+
+            //create a random list of 10 integers and output to console
+            Console.WriteLine("The original list is");
             List<int> rndList=GenerateList(10,0,100);
-            
+            foreach (int i in rndList){
+                Console.Write($"{i}, ");    
+            }
+            Console.WriteLine("\nSorted by Bubble sort \n");
+            List<int> bubbleList=MyBubbleSort(rndList);
+            foreach (int i in bubbleList){
+                Console.Write($"{i}, ");    
+            }
+            Console.WriteLine("\nSorted by Merge sort \n");
+            List<int> mergeList=MyMergeSort(rndList);
+            foreach (int i in mergeList){
+                Console.Write($"{i}, ");    
+            }
+
         }
 
 
